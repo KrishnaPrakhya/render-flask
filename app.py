2eedb17a37c76987d48a3a4100141a6fa1bff211
KrishnaPrakhya <krishnasaiprakhya@gmail.com>
2025-05-17 23:26:42 +0530

    some changes

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app, resources={
     r"/api/*": {
-        "origins": ["http://localhost:3000"],
+        "origins": ["http://localhost:3000","https://elevate-ai-flask.onrender.com"],
         "methods": ["GET", "POST", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"],
         "supports_credentials": True
